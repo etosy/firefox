0df01b5b327fa8389d1666f458ef67c421613919
Celestine Dias <celestinediask@gmail.com>
2024-02-15 15:58:30 +0530

    arkenfox.sh file updated
    made it user friedly and more automated

--- arkenfox.sh
+++ arkenfox.sh
@@ -3,50 +3,88 @@
 # exit if error occurs
 set -e
 
-# exit if dependecy packages are not installed
-if ! which wget >> /dev/null; then echo "wget not installed. Exiting."; exit; fi
-if ! which curl >> /dev/null; then echo "curl not installed. Exiting."; exit; fi
-if ! which unzip >> /dev/null; then echo "unzip not installed. Exiting."; exit; fi
+# uncomment to enable debug output
+#set -x
 
+# exit if snap not installed
+if ! command -v snap &> /dev/null; then
+    echo "Error: snap is not installed. Please install snap to use this script. Exiting."
+    exit 1
+fi
+
+# exit if snap firefox not installed
+if ! snap list | grep -q firefox; then
+    echo "Error: Snap Firefox is not installed. Please install Snap Firefox to use this script."
+    exit 1
+fi
+
+# get profile name as arguement
 profile=$1
 
-# exit if no arguement is given
-if [ -z $profile ];then echo "Profile name not given! Exiting."; exit 1; fi
+# if profile name is not given as arguent get from the user
+if [ -z "$profile" ]; then
+    # Prompt the user for the profile name
+    read -p "Enter the profile name: " profile
 
-# exit if firefox not installed
-if ! command -v firefox >> /dev/null; then echo "Firefox is not installed. Exiting."; exit 1; fi
+    # Check again if the user provided a profile name
+    if [ -z "$profile" ]; then
+        echo "Profile name not given! Exiting."
+        exit 1
+    fi
+fi
 
-# exit if default profile exists
-if [ -d ~/.mozilla/firefox/*default* ] || [ -d ~/.cache/mozilla/firefox/*default* ]; then echo "Default Firefox profile found. Exiting."; exit 1; fi
+# exit if given profile already exists
+# debian path: ~/.mozilla/firefox/*$profile
+if [ -d ~/snap/firefox/common/.mozilla/firefox/*$profile ]; then
+    echo "There is already a profile titled as $profile exists. Exiting."
+    exit 1
+fi
 
-# exit if there is already a profile titled as given argument exists
-if [ -d ~/.mozilla/firefox/*$profile ]; then echo "There is already a profile titled as $profile exists. Exiting."; exit 1; fi
+# exit if not internet
+print_color() {
+    color="$1"
+    message="$2"
+    case "$color" in
+        "green") echo -e "\e[32m$message\e[0m";;  # Green color
+        "red") echo -e "\e[31m$message\e[0m";;    # Red color
+        *) echo "$message";;
+    esac
+}
 
-# exit if no internet
-if ! ping -c 1 example.com >> /dev/null; then echo "No internet! Exiting."; exit 1; fi
+if ping -c 1 google.com > /dev/null 2>&1; then
+    print_color "green" "Internet connection is available."
+else
+    print_color "red" "No internet! Exiting."
+    exit 1
+fi
 
 
-zip_ver=$(curl -sL https://github.com/arkenfox/user.js/releases/latest | grep -oE '[0-9]+\.[0-9]+' | sed -n '1p')
-url=github.com/arkenfox/user.js/archive/refs/tags/$zip_ver.zip
-zip=$(basename $url)
+latest_release_ver=$(curl -sL https://github.com/arkenfox/user.js/releases/latest | grep -oE '[0-9]+\.[0-9]+' | sed -n '1p')
 
-wget -nc -q -L $url
-unzip -n -q $zip
+url="github.com/arkenfox/user.js/archive/refs/tags/$latest_release_ver.tar.gz"
 
-firefox --CreateProfile $profile
+tar_file=$(basename "$url")
+
+curl -O -s -L $url
+
+tar -xf $tar_file
+
+firefox --CreateProfile $profile >>/dev/null 2>&1
 firefox -offline --headless -p $profile &>> /dev/null &
 sleep 6
 pkill firefox
 sleep 1
 
-profile_dir=$(basename $(ls -d ~/.mozilla/firefox/*$profile))
+profile_dir=$(basename $(ls -d ~/snap/firefox/common/.mozilla/firefox/*$profile))
 
-cd user.js-$zip_ver/
-cp -i prefsCleaner.sh updater.sh user.js ~/.mozilla/firefox/*$profile_dir
-cd ~/.mozilla/firefox/*$profile_dir
+cd user.js-$latest_release_ver/
+cp -i prefsCleaner.sh updater.sh user.js ~/snap/firefox/common/.mozilla/firefox/*$profile_dir
+cd ~/snap/firefox/common/.mozilla/firefox/*$profile_dir
 echo 'user_pref("keyword.enabled", true);
 user_pref("media.peerconnection.enabled", false);
 user_pref("privacy.resistFingerprinting.letterboxing", false);' >> user-overrides.js
 ./updater.sh -u -s >> /dev/null
 ./prefsCleaner.sh -s >> /dev/null
 cd - >> /dev/null
+
+firefox -p $profile >>/dev/null 2>&1 &
